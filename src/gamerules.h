e2047e7dcf9aa3da9d2b160b19aad9a79e1662a2
albailey <albailey1974@gmail.com>
2013-08-13 21:08:49 -0400

    Alter level loading code in preparation for doors

--- src/gamerules.h
+++ src/gamerules.h
@@ -1,8 +1,11 @@
 
 #define GRAVITY_VELOCITY  2
-
 #define INITIAL_JUMP_VELOCITY  2
 #define REDUCED_JUMP_VELOCITY  1
-#define JUMP_DURATION     8
+#define JUMP_DURATION     12
+
+#define PORTAL_DURATION     16
+
+#define STANDING_COUNTER    32
 
 
--- src/level1.h
+++ src/level1.h
@@ -16,4 +16,10 @@ extern const unsigned char* LEVEL1_TILES_ADDR;
 #define LEVEL1_COLUMNS_BANK 5
 extern const unsigned char* LEVEL1_COLUMNS_ADDR;
 
+// When the level loads, this indicates where in the level the player appears
+#define LEVEL1_START_X 40
+#define LEVEL1_START_Y 140
+#define LEVEL1_START_COLUMN 0
+#define LEVEL1_START_SCROLL 0
+
 
